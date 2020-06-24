900099100c7645b7a858aeea325103c7f44f24ec
Joao Gouveia <joao.gouveia@criticaltechworks.com>
2020-06-24 11:31:47 +0100

    distribution calculation page layout is almost ok, it is missing do the money mask and javascript validations

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,4 +4,7 @@ class PagesController < ApplicationController
 
  def about
  end
+
+ def distribution_calculation
+ end
 end
---
+++ app/javascript/custom/distribution_calculator.js
@@ -0,0 +1,76 @@
+(function($) {
+    $.fn.invisible = function() {
+        return this.each(function() {
+            $(this).css("visibility", "hidden");
+        });
+    };
+    $.fn.visible = function() {
+        return this.each(function() {
+            $(this).css("visibility", "visible");
+        });
+    };
+}(jQuery));
+
+function handleDismiss(){
+	$("#erroralert").invisible();
+}
+
+function handleClick(){
+
+	var entradaDeCaixa = $("#input-entrada-de-caixa").val();
+	var emprestimosJoao = $("#input-emprestimos-joao").val();
+	var emprestimosAndressa = $("#input-emprestimos-andressa").val();
+
+	if(entradaDeCaixa.length == 0){
+		entradaDeCaixa = 0;
+	}else{
+		entradaDeCaixa = entradaDeCaixa.replace(",", "");
+	}
+
+	if(emprestimosJoao.length == 0){
+		emprestimosJoao = 0;
+	}else{
+		emprestimosJoao = emprestimosJoao.replace(",", "");
+	}
+
+	if(emprestimosAndressa.length == 0){
+		emprestimosAndressa = 0;
+	}else{
+		emprestimosAndressa = emprestimosAndressa.replace(",", "");
+	}
+
+	if(entradaDeCaixa == 0){
+		$("#erroralert").visible();
+		return;
+	}
+
+	$("#erroralert").invisible();
+	$("#results").show();
+	$("#bt_calculate").prop('disabled', true);
+	$("#input-entrada-de-caixa").prop('disabled', true);
+	$("#input-emprestimos-joao").prop('disabled', true);
+	$("#input-emprestimos-andressa").prop('disabled', true);
+
+	var fairSplit = Math.floor((entradaDeCaixa / 3) * 100) / 100;
+	
+	var casaSlice = fairSplit + parseFloat(emprestimosJoao) + parseFloat(emprestimosAndressa);
+	casaSlice = Math.floor(casaSlice * 100) / 100;
+	var joaoSlice = fairSplit - parseFloat(emprestimosJoao);
+	var andressaSlice = fairSplit - parseFloat(emprestimosAndressa);
+	$("#casaresult").append("\u20ac " + casaSlice);
+	$("#joaoresult").append("\u20ac " + joaoSlice);
+	$("#andressaresult").append("\u20ac " + andressaSlice);
+}
+
+function moneymask(){
+	console.log("test");
+}
+
+$(document).ready(function(){
+	$(".money-input").on("keydown", moneymask);
+	$("#bt_calculate").click(handleClick);
+	$("#results").hide();
+	$("#erroralert").invisible();
+	$("#alertDismiss").click(handleDismiss);
+});
+
