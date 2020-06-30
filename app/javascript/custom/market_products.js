e1fc2e567837e66c60f5dea2b9ff7b7ced8fbfeb
Joao Gouveia <joao.gouveia@criticaltechworks.com>
2020-06-30 20:03:06 +0100

    product list filtering with js

--- app/javascript/custom/market_products.js
+++ app/javascript/custom/market_products.js
@@ -9,4 +9,19 @@ $(document).on('turbolinks:load',function(){
 	 		}
 	 	});
 	 });
+
+	 $("#searchField").keyup(function(){
+	 	//thanks to https://www.w3schools.com/howto/howto_js_filter_table.asp
+	 	var query = $(this).val();
+	 	var table = document.getElementById("productTable");
+	 	var rows = table.getElementsByTagName("tr");
+	 	for(i = 1; i < rows.length; i++){
+	 		var productName = rows[i].getElementsByTagName("td")[0].innerText;
+	 		if (productName.toUpperCase().indexOf(query.toUpperCase()) > -1) {
+        		rows[i].style.display = "";
+      		} else {
+        		rows[i].style.display = "none";
+      		}
+	 	}
+	 })
 });
